f66ebb6938bfb7aca13a654f0966c99411a36635
Raven-github <15733294296@163.com>
2018-07-12 19:39:51 +0800

    修改

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e5/60f12a0ac88500181423c124fcd0d4c5
@@ -0,0 +1,10 @@
+package link.list;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ Offers/src/offers/TreeNode.java
@@ -0,0 +1,12 @@
+package offers;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
